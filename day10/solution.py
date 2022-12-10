c4261cf1ae42bd5e82e54600a9e9b90bc789e688
jaakkoniemensivu <jaakko.v.niemensivu@gmail.com>
2022-12-10 18:23:46 +0200

    Add day10

---
+++ day10/solution.py
@@ -0,0 +1,39 @@
+f = open("data.txt")
+
+x_register = 1
+cycle = 0
+signal_strengths = []
+# p2
+crt_drawing = ""
+
+for l in f.readlines():
+    l = l.strip().split(" ")
+
+    if len(l) == 1:
+        cycles_added = 1
+        v = 0
+    else:
+        v = int(l[1])
+        cycles_added = 2
+
+    for i in range(cycles_added):
+        cycle += 1
+
+        if cycle <= 220 and cycle % 40 == 20:
+            signal_strengths.append(cycle * x_register)
+
+        # p2
+        pos = (cycle - 1) % 40
+        
+        pixel = "█" if pos in range(x_register - 1, x_register + 2) else "."
+        crt_drawing += pixel
+
+        if (cycle == 200): print(pixel, pos, x_register)
+
+        if pos == 39: crt_drawing += "\n"
+            
+
+    x_register += v
+
+# print(sum(signal_strengths))
+print(crt_drawing)
